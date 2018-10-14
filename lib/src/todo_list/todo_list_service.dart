d36032146d7d75503d3cafa8ce29ef7331187bfd
Americo Tomas <americo.tomas@fuseuniversal.com>
2018-10-14 09:26:54 +0100

    addind some hardcoded default values

--- lib/src/todo_list/todo_list_service.dart
+++ lib/src/todo_list/todo_list_service.dart
@@ -2,10 +2,22 @@ import 'dart:async';
 
 import 'package:angular/core.dart';
 
+Iterable<String> thingsTodo() sync* {
+  var actions = ['Walk', 'Wash', 'Feed'];
+  var pets = ['cats', 'dogs'];
+
+  for (var action in actions) {
+    for (var pet in pets) {
+      if (pet == 'cats' && action != 'Feed') continue;
+      yield '$action the $pet';
+    }
+  }
+}
+
 /// Mock service emulating access to a to-do list stored on a server.
 @Injectable()
 class TodoListService {
-  List<String> mockTodoList = <String>[];
+  List<String> mockTodoList = thingsTodo().toList();
 
   Future<List<String>> getTodoList() async => mockTodoList;
 }
