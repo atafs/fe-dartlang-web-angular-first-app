a389284593ec1f2e65571d9cf033d7af0d828d32
Americo Tomas <americo.tomas@fuseuniversal.com>
2018-10-14 08:41:14 +0100

    start the project

---
+++ web/main.dart
@@ -0,0 +1,6 @@
+import 'package:angular/angular.dart';
+import 'package:my_first_dartlang_angular_web_app/app_component.template.dart' as ng;
+
+void main() {
+  runApp(ng.AppComponentNgFactory);
+}
